07618151336204430c46757c496150a7be324ed1
Feda Almodhi <xoitsfee@gmail.com>
2025-07-01 22:26:46 -0400

    attempt

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-vite-portfolio/', 
+  base: '/3D-PORTFOLIO/', 
   plugins: [react(), tailwindcss()],
 })
