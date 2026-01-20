f165665b90019ab6319722ca204fe501b265dc51
Feda Almodhi <143366228+floatingsanity@users.noreply.github.com>
2026-01-20 22:58:23 +0300

    order updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,76 +1,7 @@
 export const myProjects = [
+  
   {
-    id: 1,
-    title: "Ventures – Group Travel Planning App",
-    description:
-      "A comprehensive capstone project that simplifies collaborative travel planning with budgeting, AI suggestions, and location sharing.",
-    subDescription: [
-      "Led UX design and research for a multi-feature travel planning app, 'Ventures', with AI-enhanced tools and real-time collaboration.",
-      "Created wireframes, user personas, and high-fidelity Figma prototypes based on user interviews and usability testing.",
-      "Developed core features like shared itineraries, expense tracking, and decentralized transaction concepts.",
-      "Focused on design systems and accessibility while presenting the product at final review panels.",
-    ],
-    href: "https://www.figma.com/design/z4wQwFY7kZFsBdXxvdnSIa/HCDD-440-Prototype?node-id=231-345&t=FWDmms7qvPPBuOzZ-1", 
-    logo: "",
-    image: "assets/Ventures.jpeg", 
-    tags: [
-      { id: 1, name: "UX Design" },
-      { id: 2, name: "Design Systems" },
-      { id: 3, name: "AI Integration" },
-      { id: 4, name: "Product Strategy" },
-      { id: 5, name: "Figma Prototypes" },
-      { id: 6, name: "Travel Planning" },
-    ],
-  },
-  {
-    id: 2,
-    title: "LMS – Learning Management System Scheduler",
-    description:
-      "A Java-based system improving course scheduling, advisor interaction, and conflict resolution in academic environments.",
-    subDescription: [
-      "Designed and implemented a backend-heavy LMS using Java and object-oriented design principles.",
-      "Used the Strategy Pattern and Wizard Pattern to optimize scheduling flow and user experience.",
-      "Built UML diagrams, use case flows, and conducted user testing for role-based dashboards.",
-      "Delivered a fully functional IntelliJ-based application supporting multiple stakeholder roles (admin, student, advisor).",
-    ],
-    href: "https://github.com/DavidOke02/coursescheduling_project", 
-    logo: "",
-    image: "assets/LMS.png",
-    tags: [
-    { id: 1, name: "Java" },
-    { id: 2, name: "Object-Oriented Design" },
-    { id: 3, name: "Design Patterns" },
-    { id: 4, name: "Scheduling Algorithms" },
-    { id: 5, name: "Information Architecture" },
-    { id: 6, name: "UML Diagrams" },
-  ],
-  },
-  {
-    id: 3,
-    title: "Lucid – Narrative Horror Game ",
-    description:
-      "This psychological horror game traps players in an endless loop of surreal dreams. Players complete mysterious tasks, collect scattered clocks, and evade shadowy figures to reclaim their bodies and awaken. The game was published by my teammate Jana. You can view our individual contributions on our GitHub repository: https://github.com/JanaAlqarni-lab/Dreams",
-    subDescription: [
-      "Designed an interactive 3D game environment in Unity, emphasizing psychological tension and surreal dreamscapes.",
-      "Implemented core gameplay mechanics including object interaction, clock collection, and dynamic shadow-figure evasion.",
-      "Crafted a branching narrative exploring themes of dissociation, identity, and reclaiming agency through surreal visual and gameplay metaphors.",
-      "Modeled and animated original assets in Blender, integrated with Godot to create immersive, looping dream sequences.",
-    ],
-    href: "https://jana-04.itch.io/lucid", 
-    logo: "",
-    image: "assets/lucid.png", 
-    tags: [
-    { id: 1, name: "Unity Engine" },
-    { id: 2, name: "Game Design" },
-    { id: 3, name: "Narrative Design" },
-    { id: 4, name: "Psychological Horror" },
-    { id: 5, name: "3D Environment" },
-    { id: 6, name: "Blender" },
-    { id: 7, name: "Storytelling" },
-  ],  
-  },
-{
-  id: 4,
+  id: 1,
   title: "ChartEx - FinTech Workflow & n8n Automation",
   description:
     "ChartEx is a FinTech project where I implement and manage automated workflows using n8n, handling separate development and production environments with Supabase databases. The system ensures safe feature development, structured migrations, and controlled deployments, enabling reliable and scalable operations. You can view the project on our GitHub repository: https://github.com/YourUsername/ChartEx",
@@ -93,7 +24,76 @@ export const myProjects = [
     { id: 6, name: "CI/CD" },
     { id: 7, name: "Database Management" }
   ],
-}
+}, {
+    id: 2,
+    title: "Lucid – Narrative Horror Game ",
+    description:
+      "This psychological horror game traps players in an endless loop of surreal dreams. Players complete mysterious tasks, collect scattered clocks, and evade shadowy figures to reclaim their bodies and awaken. The game was published by my teammate Jana. You can view our individual contributions on our GitHub repository: https://github.com/JanaAlqarni-lab/Dreams",
+    subDescription: [
+      "Designed an interactive 3D game environment in Unity, emphasizing psychological tension and surreal dreamscapes.",
+      "Implemented core gameplay mechanics including object interaction, clock collection, and dynamic shadow-figure evasion.",
+      "Crafted a branching narrative exploring themes of dissociation, identity, and reclaiming agency through surreal visual and gameplay metaphors.",
+      "Modeled and animated original assets in Blender, integrated with Godot to create immersive, looping dream sequences.",
+    ],
+    href: "https://jana-04.itch.io/lucid", 
+    logo: "",
+    image: "assets/lucid.png", 
+    tags: [
+    { id: 1, name: "Unity Engine" },
+    { id: 2, name: "Game Design" },
+    { id: 3, name: "Narrative Design" },
+    { id: 4, name: "Psychological Horror" },
+    { id: 5, name: "3D Environment" },
+    { id: 6, name: "Blender" },
+    { id: 7, name: "Storytelling" },
+  ],  
+  },
+  {
+    id: 3,
+    title: "LMS – Learning Management System Scheduler",
+    description:
+      "A Java-based system improving course scheduling, advisor interaction, and conflict resolution in academic environments.",
+    subDescription: [
+      "Designed and implemented a backend-heavy LMS using Java and object-oriented design principles.",
+      "Used the Strategy Pattern and Wizard Pattern to optimize scheduling flow and user experience.",
+      "Built UML diagrams, use case flows, and conducted user testing for role-based dashboards.",
+      "Delivered a fully functional IntelliJ-based application supporting multiple stakeholder roles (admin, student, advisor).",
+    ],
+    href: "https://github.com/DavidOke02/coursescheduling_project", 
+    logo: "",
+    image: "assets/LMS.png",
+    tags: [
+    { id: 1, name: "Java" },
+    { id: 2, name: "Object-Oriented Design" },
+    { id: 3, name: "Design Patterns" },
+    { id: 4, name: "Scheduling Algorithms" },
+    { id: 5, name: "Information Architecture" },
+    { id: 6, name: "UML Diagrams" },
+  ],
+  },
+{
+    id: 4,
+    title: "Ventures – Group Travel Planning App",
+    description:
+      "A comprehensive capstone project that simplifies collaborative travel planning with budgeting, AI suggestions, and location sharing.",
+    subDescription: [
+      "Led UX design and research for a multi-feature travel planning app, 'Ventures', with AI-enhanced tools and real-time collaboration.",
+      "Created wireframes, user personas, and high-fidelity Figma prototypes based on user interviews and usability testing.",
+      "Developed core features like shared itineraries, expense tracking, and decentralized transaction concepts.",
+      "Focused on design systems and accessibility while presenting the product at final review panels.",
+    ],
+    href: "https://www.figma.com/design/z4wQwFY7kZFsBdXxvdnSIa/HCDD-440-Prototype?node-id=231-345&t=FWDmms7qvPPBuOzZ-1", 
+    logo: "",
+    image: "assets/Ventures.jpeg", 
+    tags: [
+      { id: 1, name: "UX Design" },
+      { id: 2, name: "Design Systems" },
+      { id: 3, name: "AI Integration" },
+      { id: 4, name: "Product Strategy" },
+      { id: 5, name: "Figma Prototypes" },
+      { id: 6, name: "Travel Planning" },
+    ],
+  }
 ];
 
 export const mySocials = [
